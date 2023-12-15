4eb0cd7edf75edd30e7826b8895f956f1444bb13
Mario Zechner <badlogicgames@gmail.com>
2023-12-15 18:16:44 +0100

    Retry db connection for 15 seconds

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,9 +8,10 @@ import * as http from "http";
 import multer from "multer";
 import WebSocket, { WebSocketServer } from "ws";
 import { Pool } from "pg";
+import { sleep } from "../utils/utils.js";
 const upload = multer({ storage: multer.memoryStorage() });
 
-const port = process.env.PORT ?? 3333; //
+const port = process.env.PORT ?? 3333;
 const dbName = process.env.DATABASE;
 if (!dbName) {
     console.error("Environment variable DATABASE missing");
@@ -36,16 +37,9 @@ const pool = new Pool({
 });
 
 (async () => {
-    try {
-        const client = await pool.connect();
-        try {
-            const result = await client.query("SELECT NOW()");
-            console.log("Query result:", result.rows);
-        } finally {
-            client.release(); // Release the client back to the pool
-        }
-    } catch (err) {
-        console.error("Error during database connection or query", err);
+    const result = connectWithRetry(5, 3000);
+    if (result instanceof Error) {
+        process.exit(-1);
     }
 
     if (!fs.existsSync("docker/data")) {
@@ -69,23 +63,47 @@ const pool = new Pool({
     });
 
     setupLiveReload(server);
-    function setupLiveReload(server: http.Server) {
-        const wss = new WebSocketServer({ server });
-        const clients: Set<WebSocket> = new Set();
-        wss.on("connection", (ws: WebSocket) => {
-            clients.add(ws);
-            ws.on("close", () => {
-                clients.delete(ws);
-            });
-        });
-
-        chokidar.watch("html/", { ignored: /(^|[\/\\])\../, ignoreInitial: true }).on("all", (event, path) => {
-            clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(`File changed: ${path}`);
-                }
-            });
-        });
-        console.log("Initialized live-reload");
-    }
 })();
+
+async function connectWithRetry(maxRetries = 5, interval = 2000) {
+    let retries = 0;
+    while (retries < maxRetries) {
+        try {
+            const client = await pool.connect();
+            try {
+                const result = await client.query("SELECT NOW()");
+                console.log("Query result:", result.rows);
+                return undefined; // Successful connection, exit the function
+            } finally {
+                client.release();
+            }
+        } catch (err) {
+            console.error("Connection attempt failed:", err);
+            retries++;
+            if (retries === maxRetries) {
+                return new Error("Failed to connect to the database after retries");
+            }
+            await sleep(interval);
+        }
+    }
+}
+
+function setupLiveReload(server: http.Server) {
+    const wss = new WebSocketServer({ server });
+    const clients: Set<WebSocket> = new Set();
+    wss.on("connection", (ws: WebSocket) => {
+        clients.add(ws);
+        ws.on("close", () => {
+            clients.delete(ws);
+        });
+    });
+
+    chokidar.watch("html/", { ignored: /(^|[\/\\])\../, ignoreInitial: true }).on("all", (event, path) => {
+        clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`File changed: ${path}`);
+            }
+        });
+    });
+    console.log("Initialized live-reload");
+}
